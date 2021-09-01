e66b47fa99089352bd74f26c93ee0e88f733258f
KaterinaGerts <75572090+KaterinaGerts@users.noreply.github.com>
2021-09-01 19:52:51 +0300

    переписала модалку на хуки

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,39 +1,71 @@
-import React, { Component } from 'react';
+import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import s from './Modal.module.css';
 
 const modalRoot = document.querySelector('#modal-root');
 
-export default class Modal extends Component {
-  onEscapeClick = e => {
+function Modal({ modalImage, onClose }) {
+  const onEscapeClick = e => {
     if (e.code === 'Escape') {
-      this.props.onClose();
+      onClose();
     }
   };
 
-  componentDidMount() {
-    window.addEventListener('keydown', this.onEscapeClick);
-  }
+  useEffect(() => {
+    window.addEventListener('keydown', onEscapeClick);
+    return () => {
+      window.removeEventListener('keydown', onEscapeClick);
+    };
+  });
 
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.onEscapeClick);
-  }
-
-  closeOverlay = e => {
+  const closeOverlay = e => {
     if (e.currentTarget === e.target) {
-      this.props.onClose();
+      onClose();
     }
   };
 
-  render() {
-    const modalImage = this.props.modalImage;
-    return createPortal(
-      <div className={s.Overlay} onClick={this.closeOverlay}>
-        <div className={s.Modal}>
-          <img className={s.modalImage} src={modalImage} alt="" width="500" />
-        </div>
-      </div>,
-      modalRoot,
-    );
-  }
+  return createPortal(
+    <div className={s.Overlay} onClick={closeOverlay}>
+      <div className={s.Modal}>
+        <img className={s.modalImage} src={modalImage} alt="" width="500" />
+      </div>
+    </div>,
+    modalRoot,
+  );
 }
+
+export default Modal;
+
+// export default class Modal extends Component {
+//   onEscapeClick = e => {
+//     if (e.code === 'Escape') {
+//       this.props.onClose();
+//     }
+//   };
+
+//   componentDidMount() {
+//     window.addEventListener('keydown', this.onEscapeClick);
+//   }
+
+//   componentWillUnmount() {
+//     window.removeEventListener('keydown', this.onEscapeClick);
+//   }
+
+//   closeOverlay = e => {
+//     if (e.currentTarget === e.target) {
+//       this.props.onClose();
+//     }
+//   };
+
+//   render() {
+//     const modalImage = this.props.modalImage;
+//     return createPortal(
+//       <div className={s.Overlay} onClick={this.closeOverlay}>
+//         <div className={s.Modal}>
+//           <img className={s.modalImage} src={modalImage} alt="" width="500" />
+//         </div>
+//       </div>,
+//       modalRoot,
+//     );
+//   }
+// }
